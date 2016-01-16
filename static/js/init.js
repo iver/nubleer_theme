8cd65139e6b315385166aba1aba9cb0cc33f846b
Ivan Jaimes <ivan.jaimes@iver.mx>
2016-01-16 08:51:59 -0600

    Update index layout

--- static/js/init.js
+++ static/js/init.js
@@ -3,6 +3,11 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    table = $('table');
+    table.attr({ class:"dataTable", role:"grid", "aria-describedby":"data-table-simple_info" });
+    table.find('tr').attr({ role:"row" })
+    table.find('tbody tr:odd').attr({ class:"odd"});
+    table.find('tbody tr:not(.odd)').attr({ class:"even"});
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
